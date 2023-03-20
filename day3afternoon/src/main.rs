7c53c53c57e5b0bcaf2de20c0205668b8c331985
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-03-20 09:36:58 +0900

    Add day 3 afternoon

---
+++ day3afternoon/src/main.rs
@@ -0,0 +1,83 @@
+#![allow(unused_imports)]
+
+mod ffi {
+    use std::os::raw::{c_char, c_int, c_long, c_ulong, c_ushort};
+
+    // Opaque type. See https://doc.rust-lang.org/nomicon/ffi.html.
+    #[repr(C)]
+    pub struct DIR {
+        _data: [u8; 0],
+        _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+    }
+
+    // Layout as per readdir(3) and definitions in /usr/include/x86_64-linux-gnu.
+    #[repr(C)]
+    pub struct dirent {
+        pub d_ino: c_long,
+        pub d_off: c_ulong,
+        pub d_reclen: c_ushort,
+        pub d_type: c_char,
+        pub d_name: [c_char; 256],
+    }
+
+    extern "C" {
+        pub fn opendir(s: *const c_char) -> *mut DIR;
+        pub fn readdir(s: *mut DIR) -> *const dirent;
+        pub fn closedir(s: *mut DIR) -> c_int;
+    }
+}
+
+use std::ffi::{CStr, CString, OsStr, OsString};
+use std::fmt::format;
+use std::os::unix::ffi::OsStrExt;
+
+#[derive(Debug)]
+struct DirectoryIterator {
+    path: CString,
+    dir: *mut ffi::DIR,
+}
+
+impl DirectoryIterator {
+    fn new(path: &str) -> Result<DirectoryIterator, String> {
+        // Call opendir and return a Ok value if that worked,
+        // otherwise return Err with a message.
+        let path = CString::new(path).map_err(|err| format!("Invalid input path: {err}"))?;
+        let dir = unsafe { ffi::opendir(path.as_ptr()) };
+        if dir.is_null() {
+            Err(format!("Directory {:?} doesn't exists", path))
+        } else {
+            Ok(DirectoryIterator { path, dir })
+        }
+    }
+}
+
+impl Iterator for DirectoryIterator {
+    type Item = OsString;
+    fn next(&mut self) -> Option<OsString> {
+        // Keep calling readdir until we get a NULL pointer back.
+        let dir = unsafe { ffi::readdir(self.dir) };
+        if dir.is_null() {
+            return None;
+        }
+        let directory_name = unsafe { CStr::from_ptr((*dir).d_name.as_ptr()) };
+        let result = OsStr::from_bytes(directory_name.to_bytes());
+        Some(result.to_owned())
+    }
+}
+
+impl Drop for DirectoryIterator {
+    fn drop(&mut self) {
+        // Call closedir as needed.
+        if !self.dir.is_null() {
+            if unsafe { ffi::closedir(self.dir) } != 0 {
+                panic!("Could not close {:?}", self.path);
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), String> {
+    let iter = DirectoryIterator::new(".")?;
+    println!("files: {:#?}", iter.collect::<Vec<_>>());
+    Ok(())
+}
